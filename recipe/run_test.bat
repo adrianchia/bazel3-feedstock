0df3c582e3b6535e1f10e069b346c883d5be501a
Adrian Chia <achia@adrianchia.com>
2020-10-14 17:26:58 -0500

    Test

--- recipe/run_test.bat
+++ recipe/run_test.bat
@@ -1,10 +1,10 @@
 @echo on
 
 mkdir tutorial
-xcopy /e /s /t %RECIPE_DIR%\tutorial tutorial
+ROBOCOPY %RECIPE_DIR%\tutorial tutorial /E
 cd tutorial
 set BAZEL_BUILD_OPTS=""
 set "EXTRA_BAZEL_ARGS=--host_javabase=@local_jdk//:jdk"
 set "BAZEL_VS=%VSINSTALLDIR%"
-DIR
+
 bazel build "%{BAZEL_BUILD_OPTS%" //main:hello-world
