ee7abf4a7b0b80611f4271277826c9149ff904c2
Adrian Chia <achia@adrianchia.com>
2020-10-14 11:53:59 -0500

    Add test

--- recipe/run_test.bat
+++ recipe/run_test.bat
@@ -1,6 +1,7 @@
 @echo on
 
-xcopy /e /s /t %RECIPE_DIR%\tutorial .\tutorial
+mkdir tutorial
+xcopy /e /s /t %RECIPE_DIR%\tutorial tutorial
 cd tutorial
 set BAZEL_BUILD_OPTS=""
 bazel build "%{BAZEL_BUILD_OPTS%" ///main:hello-world
